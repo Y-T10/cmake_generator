225f602e550d307144294c63ca54d2c09fecc2d0
Y-T10 <t06t06ca2@gmail.com>
2023-03-14 01:44:18 +0900

    レンダリング環境生成を変更

--- src/CompRender/CmpRenderRendering.cpp
+++ src/CompRender/CmpRenderRendering.cpp
@@ -7,9 +7,18 @@
 using namespace std::filesystem;
 using namespace inja;
 
+namespace {
+    const Environment CreateCustomEnv() noexcept {
+        Environment env;
+        env.set_lstrip_blocks(true);
+        env.set_trim_blocks(true);
+        return env;
+    };
+}
+
 namespace CompRender {
 std::ostream& RenderText(std::ostream& out, const  std::string& templateName, const json& props) noexcept {
-    Environment env;
+    auto env = CreateCustomEnv();
     const auto tpl =  env.parse_template(current_path() / "template" / templateName);
     return env.render_to(out, tpl, props);
 }
