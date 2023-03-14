351f08bbe1357e051c11331e7519c52ab5af06ea
Y-T10 <tayuji111@gmail.com>
2023-03-14 21:01:41 +0900

    オプションの実装途中

--- src/AppMain/AppMainMain.cpp
+++ src/AppMain/AppMainMain.cpp
@@ -3,9 +3,13 @@
 #include <iostream>
 #include <cstdint>
 #include "fmt/format.h"
+#include "AppMainOptions.hpp"
+#include <filesystem>
+#include <cassert>
 
 using namespace std;
 using namespace inja;
+using namespace std::filesystem;
 
 // セマンティックバージョンを表す型
 struct Version {
@@ -37,7 +41,27 @@ const json CreateProjectParam(const string& name, const Version& version, const 
     return projectProps;
 };
 
+// main関数のパラメータをパースする
+const AppMain::AppOption ParseAppArgs(const int argc, const char* argv[]) noexcept {
+    assert(argc >= 0);
+    const string programName = (argc > 0)? "tcm": path(argv[0]).filename().string();
+    const int paramArgc = (argc > 0)? argc - 1: 0;
+    const char** paramArgv = (argc > 0)? (argv + 1): argv;
+    assert(paramArgv[paramArgc] == nullptr);
+    return AppMain::ParseOptions(programName, "テンプレートファイルからCMakeファイルを生成する．", paramArgc, paramArgv);
+};
+
+const cxxopts::Options CreateAppArgParser(const int argc, const char* argv[]) noexcept {
+    
+}
+
 int main(int argc, char* argv[]) {
+    const auto opt = ParseOptions();
+    const auto args = ParseAppArgs(argc, argv);
+
+    if(args.count("h")){
+    }
+
     CompRender::RenderText(std::cout, "project.tpl", json::object({
         {"cmakeVersionMin", toString({0,0,0})},
         {"project", CreateProjectParam("", {0,0,0}, "CXX", "Release")}
