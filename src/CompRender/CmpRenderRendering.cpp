592dc8b1c194be45efa5db0137e79a27ca85ff82
Y-T10 <t06t06ca2@gmail.com>
2023-03-16 22:12:14 +0900

    テンプレートのファイル名をテンプレート名から得るようにした

--- src/CompRender/CmpRenderRendering.cpp
+++ src/CompRender/CmpRenderRendering.cpp
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <optional>
 #include <ostream>
+#include "fmt/format.h"
 
 using namespace std::filesystem;
 using namespace inja;
@@ -19,7 +20,8 @@ namespace {
 namespace CompRender {
 std::ostream& RenderText(std::ostream& out, const  std::string& templateName, const json& props) noexcept {
     auto env = CreateCustomEnv();
-    const auto tpl =  env.parse_template(current_path() / "template" / templateName);
+    const auto filename = fmt::format(FMT_STRING("{:s}.tpl"), templateName);
+    const auto tpl =  env.parse_template(current_path() / "template" / filename);
     return env.render_to(out, tpl, props);
 }
 };
