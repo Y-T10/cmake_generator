6c51b4f1d054bb18238912186aceac347086a1b4
Y-T10 <t06t06ca2@gmail.com>
2023-03-18 20:44:13 +0900

    ディレクトリ内から指定条件に合うファイル名を探す関数を実装

---
+++ src/AppCodeGen/AppCGListFiles.cpp
@@ -0,0 +1,25 @@
+#include "AppCGListFiles.hpp"
+
+using namespace std;
+using namespace std::filesystem;
+using namespace inja;
+
+namespace AppCGListFiles {
+const optional<json::array_t>  ListFilesInDir(const path& dir, const regex& reg) noexcept {
+    if(!is_directory(dir)){
+        return nullopt;
+    }
+
+    json::array_t targetFiles = {};
+    for (const directory_entry& file : directory_iterator(dir)) {
+        if(!file.is_regular_file()){
+            continue;
+        }
+        if(!regex_match(file.path().filename().string(), reg)){
+            continue;
+        }
+        targetFiles.push_back(file.path().filename().string());
+    }
+    return targetFiles;
+}
+}
