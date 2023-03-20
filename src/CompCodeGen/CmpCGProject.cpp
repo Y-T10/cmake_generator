be5084ea094529e99ac2058c37294f223e5699f9
Y-T10 <t06t06ca2@gmail.com>
2023-03-20 12:41:12 +0900

    CMakeバージョンの既定値をDefaultCMakeVersionで決めるようにした

--- src/CompCodeGen/CmpCGProject.cpp
+++ src/CompCodeGen/CmpCGProject.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "CmpRenderRendering.hpp"
 #include "CmpCGUtility.hpp"
+#include "CmpConfCMake.hpp"
 
 using namespace cxxopts;
 using namespace inja;
@@ -29,18 +30,15 @@ const json CreateProjectParam(const ParseResult& result) noexcept {
 
 namespace CmpCG {
 void OptionProj(Options& opt) noexcept {
+    const string defCMakeVer = string(CmpConf::DefaultCMakeVersion());
     opt.add_options("project")
-        ("root", "specify root project with CMake version", value<string>()->default_value(""))
+        ("root", "specify root project with CMake version", value<string>()->default_value(defCMakeVer))
         ("ver", "project version", value<string>()->default_value("0.0.0"))
         ("lang", "project programming language", value<string>()->default_value("CXX"))
         ("bt", "project default build type", value<string>()->default_value("Release"));
 }
 
 const std::optional<inja::json>ArgParseProj(const ParseResult& result) noexcept{
-    if(result.count("root") == 0){
-        return nullopt;
-    }
-
     return json::object({
         {"cmakeVersionMin", result["root"].as<string>()},
         {"project", CreateProjectParam(result)}
