c98f37af311aeb6073f54ce853a80333f4eb3db8
Y-T10 <t06t06ca2@gmail.com>
2023-03-21 14:12:34 +0900

    不要な関数を削除

--- src/CompCodeGen/CmpCGUtility.hpp
+++ src/CompCodeGen/CmpCGUtility.hpp
@@ -10,13 +10,6 @@
 
 namespace CmpCGUtil {
     /**
-     * @brief 文字列パスの配列をpathの配列に変換する
-     * @param paths 文字列パスの配列
-     * @return 変換されたpathの配列
-     */
-    const std::vector<std::filesystem::path> ConvertToPaht(const std::vector<std::string>& paths) noexcept;
-    
-    /**
      * @brief 文字列配列をjsonの配列オブジェクトに変換する
      */
     const inja::json toJSON(const std::vector<std::string>& strings);
--- src/CompRender/CmpRenderRendering.cpp
+++ src/CompRender/CmpRenderRendering.cpp
@@ -12,58 +12,6 @@ using namespace inja;
 using namespace fmt;
 
 namespace {
-const path HomeDir() noexcept {
-    const char* homeDir = getenv("HOME");
-    if(homeDir == nullptr){
-        return "";
-    }
-    return path(homeDir);
-}
-
-const path SearchTplFile(const path& dir, const std::string& templateName) noexcept {
-    if(!exists(dir)){
-        return "";
-    }
-    for (const directory_entry& file : directory_iterator(dir)) {
-        if(!file.is_regular_file()){
-            continue;
-        }
-        if(file.path().extension().string() != ".tpl"){
-            continue;
-        }
-        if(file.path().stem() != templateName){
-            continue;
-        }
-        return file.path();
-    }
-    return "";
-}
-
-const path SearchTemplateFile(const std::string& templateName, const std::vector<path>& searchList) noexcept {
-    if(searchList.empty()){
-        return "";
-    }
-    for(const auto& dir: searchList) {
-        const auto file = SearchTplFile(dir, templateName);
-        if(!file.empty()){
-            return file;
-        }
-    }
-    return "";
-};
-
-const path FindTemplateFile(const std::string& templateName, const std::vector<path>& additionalDir) noexcept {
-    if(const auto file = SearchTemplateFile(templateName, additionalDir); !file.empty()){
-        return file;
-    }
-
-    const auto installShare = path(CmpConf::InstallPrefix()) / "share";
-    return SearchTemplateFile(templateName, {
-        current_path() / ".tpl",
-        HomeDir() / ".tcm" / "tpl",
-        installShare / "tcm" / "template"
-    });
-};
     const Environment CreateCustomEnv() noexcept {
         Environment env;
         env.set_lstrip_blocks(true);
