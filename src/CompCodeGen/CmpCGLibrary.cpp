47d55d3fc3c2c4f86dde0514c3f4606677112dbf
Y-T10 <t06t06ca2@gmail.com>
2023-03-23 22:46:13 +0900

    CmpCGTargetの関数を使いようにした

--- src/CompCodeGen/CmpCGLibrary.cpp
+++ src/CompCodeGen/CmpCGLibrary.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include "CmpRenderRendering.hpp"
 #include "CmpCGUtility.hpp"
+#include "CmpCGTarget.hpp"
 #include <filesystem>
 #include <vector>
 
@@ -12,26 +13,6 @@ using namespace cxxopts;
 using namespace inja;
 using namespace std;
 
-namespace {
-const json CreateTargetParam(const ParseResult& result) noexcept {
-    const auto sourceFiles = CmpCGUtil::ListFilesInDir(
-        result["output-dir"].as<string>(),
-        regex(R"(^[\w\-\.]+\.(c\+\+|cxx|cc|cpp)$)")
-    );
-    if(!sourceFiles.has_value()){
-        return {};
-    }
-
-    return json::object({
-        {"name", CmpCGUtil::GetName(result)},
-        {"properties", json::array({})},
-        {"sources", *sourceFiles},
-        {"includeDirs", json::array({"${CMAKE_CURRENT_SOURCE_DIR}"})},
-        {"libraries", CmpCGUtil::toJSON(result["lib"].as<std::vector<string>>())}
-    });
-};
-}
-
 namespace CmpCG {
 void OptionLib(Options& opt) noexcept {
     opt.add_options("library")
@@ -39,7 +20,7 @@ void OptionLib(Options& opt) noexcept {
 }
 
 const std::optional<inja::json>ArgParseLib(const ParseResult& result) noexcept{
-    const auto libProps = CreateTargetParam(result);
+    const auto libProps = CmpCGTarget::CreateTargetParam(result);
     if(libProps.empty()){
         return nullopt;
     }
